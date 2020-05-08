2f70ee067e73df160d6438ab15358eadc6d8afab
imnomee <59981447+imnomee@users.noreply.github.com>
2020-05-08 14:42:37 +0100

    Initial commit

---
+++ src/util/yelp.js
@@ -0,0 +1,39 @@
+const apiKey =
+	"KPIYLzx4O5fi-wA_f_cNeNAKfpSqjVqdOy972qIT5LBKdwIlvX5YZUSvXSQfQruJfktzFmLLAPGkbncF6yZKF3fjssE-3xAPA3hmziWuVnR1uSmCX--_Echasli1XnYx";
+
+const Yelp = {
+	search(term, location, sortBy) {
+		return fetch(
+			`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+			{
+				headers: {
+					Authorization: `Bearer ${apiKey}`,
+				},
+			}
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((jsonResponse) => {
+				if (jsonResponse.businesses) {
+					return jsonResponse.businesses.map((business) => {
+						console.log(business);
+						return {
+							id: business.id,
+							imageSrc: business.image_url,
+							name: business.name,
+							address: business.location.address1,
+							city: business.location.city,
+							state: business.location.state,
+							zipCode: business.location.zip_code,
+							category: business.categories[0].title,
+							rating: business.rating,
+							reviewCount: business.review_count,
+						};
+					});
+				}
+			});
+	},
+};
+
+export default Yelp;
